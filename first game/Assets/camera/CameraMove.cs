bda3a8471f9db213f41ab95aef9b713efb6da69a
imaginenation <2507483282@qq.com>
2021-05-11 19:16:15 +0800

    this is third

---
+++ first game/Assets/camera/CameraMove.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform playerTran;//主角的Transform 找到它的位置
+    public float maxDistanceX = 2;
+    public float maxDistanceY = 2;
+    public float xSpeed = 4;
+    public float ySpeed = 4;
+    public Vector2 maxXandY;
+    public Vector2 minXandY = new Vector2(-8, 8);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        playerTran = GameObject.Find//GameObject表示类，Find函数找到主角
+                ("Hero").transform;
+      //  playerTran = GameObject.FindGameObjectsWithTag("Player").transform;
+    }
+    private bool NeedMoveX()
+    {   //x方向是否需要移动摄像机
+        bool bMove = false;
+        if (Mathf.Abs(transform.position.x - playerTran.position.x) > maxDistanceX)
+            bMove = true;
+        else
+            bMove = false;
+        return bMove;
+    }
+    bool NeedMoveY()
+    {   //Y方向是否需要移动摄像机
+        bool bMove = false;
+        if (Mathf.Abs(transform.position.y - playerTran.position.y) > maxDistanceY)
+            bMove = true;
+        else
+            bMove = false;
+        return bMove;
+    }
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void TrackPlayer()
+    {
+        float newX = transform.position.x;//设置当前摄像机的位置
+        float newY = transform.position.y;
+        if (NeedMoveX()) //计算新摄像机位置
+            newX = Mathf.Lerp(transform.position.x, playerTran.position.x, xSpeed * Time.deltaTime);
+        //Lerp差值函数（当前位置，目的位置，差值） 速度xSpeed乘以时间间隔Time.deltaTime
+        if (NeedMoveY())
+            newY = Mathf.Lerp(transform.position.y,playerTran.position.y, xSpeed * Time.deltaTime);
+        newX = Mathf.Clamp(newX, minXandY.x, maxXandY.x);//固定摄像机位置
+        newY = Mathf.Clamp(newY, minXandY.y, maxXandY.y);
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+   
+}
---
+++ first game2/Assets/camera/BackGround.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour//背景运动
+{
+    // Start is called before the first frame update
+    public Transform[] backGrounds;
+    public float fParallax = 0.4f;
+    public float fParallay = 0.4f;
+    public float layerFraction = 5f;//每层之间相对运动量
+    public float fSmooth = 5f;//控制每一帧平滑程度
+    Transform cam;
+    Vector3 previousCamPos;//Start函数中赋初值
+    private void Awake()
+    {
+        cam = Camera.main.transform;//找到摄像机位置
+    }
+    private void Start()
+    {
+        previousCamPos = cam.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //x方向
+        float fParallaxX = (previousCamPos.x - cam.position.x) * fParallax;
+        for(int i=0;i<backGrounds.Length;i++)//计算每一层背景运动距离
+        {
+            float fNewX = backGrounds[i].position.x + fParallaxX * (1 + i * layerFraction);
+            Vector3 newPos = new Vector3(fNewX, backGrounds[i].position.y, backGrounds[i].position.z);//新位置
+            //所有运动都需要线性差值平滑用lerp
+            backGrounds[i].position = Vector3.Lerp(backGrounds[i].position, newPos, fSmooth * Time.deltaTime);
+        }
+        //y方向
+        float fParallaxY = (previousCamPos.y - cam.position.y) * fParallay;
+        for (int i = 0; i < backGrounds.Length; i++)//计算每一层背景运动距离
+        {
+            float fNewY = backGrounds[i].position.y + fParallaxY * (1 + i * layerFraction);
+            Vector3 newPos1 = new Vector3(backGrounds[i].position.x,fNewY, backGrounds[i].position.z);//新位置
+            //所有运动都需要线性差值平滑用lerp
+            backGrounds[i].position = Vector3.Lerp(backGrounds[i].position, newPos1, fSmooth * Time.deltaTime);
+        }
+        previousCamPos = cam.position;
+    }
+}
