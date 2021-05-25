e1dda732569113669804418dfcdd24d0b6f4a8e4
imaginenation <2507483282@qq.com>
2021-05-25 15:20:03 +0800

    this is fifth

---
+++ first game4/Assets/scrips/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float health = 100f;
+    public float hurtBloodPoint=20f;
+    SpriteRenderer healthbar;
+    Vector3 healthbarScale;
+    void Start()
+    {
+        healthbar = GameObject.Find("Health").GetComponent<SpriteRenderer>();
+        healthbarScale = healthbar.transform.localScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if(health>0)
+            {
+                //减血
+                TakeDamage();
+             
+            }
+            else
+            {
+                //播放死亡动画，掉到河里
+            }
+        }
+    }
+    void TakeDamage()
+    {
+        health -= hurtBloodPoint;
+        //更新血条状态
+        UpdateHealthBar();
+    }
+    void UpdateHealthBar()
+    {
+        healthbar.material.color = Color.Lerp(Color.green, Color.red, 1 - health * 0.01f);//血条颜色变化
+        healthbar.transform.localScale = new Vector3(health * 0.01f, 1, 1);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ first game4/Assets/scrips/Rockect1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rockect1 : MonoBehaviour
+{
+    public GameObject explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2);
+        //explosion = Resources.Load("prefabs/explosion") as GameObject; //assets文件下创建Resources文件
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        float rotationZ = Random.Range(0, 360);//产生随机数
+
+        if (collision.tag != "Player")
+        {
+
+            Instantiate(explosion, transform.position, Quaternion.Euler(new Vector3(0, 0, rotationZ)));
+            Destroy(gameObject);//销毁
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
